2db77e5de5e195a1029b7ef4b7398f09b71d0a23
Paul Peregud <paulperegud@gmail.com>
2013-07-25 16:18:36 +0200

    other way to turn off guru-mode

--- personal/no-guru.el
+++ personal/no-guru.el
@@ -1,1 +1,7 @@
+;;; Package: -- turning off Guru mode
+;;; Commentary:
+;;; turning off guru mode in a way specified at http://batsov.com/prelude/
+;;; Code:
 (setq prelude-guru nil)
+(provide 'no-guru)
+;;; no-guru ends here
